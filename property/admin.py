f6db182d32652850633a0ed7e916c29d6fa18e89
Michael Yudin <mihelitto@gmail.com>
2021-04-22 23:04:41 +0300

    Customize displayed fields on admin

--- property/admin.py
+++ property/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-
 from .models import Flat
 
+
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ["town"]
-    readonly_fields = ["created_at"]
+    search_fields = ['town']
+    readonly_fields = ['created_at']
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
+    list_editable = ['new_building']
+
 
 admin.site.register(Flat, FlatAdmin)
