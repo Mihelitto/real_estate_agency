f372b8627e4fb0888f4cb47314bb56fd782bf5f7
Michael Yudin <mihelitto@gmail.com>
2021-04-22 23:34:29 +0300

    New model for complaints

--- property/admin.py
+++ property/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from .models import Flat
+from .models import Flat, Complaint
 
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['town']
-    readonly_fields = ['created_at']
-    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
-    list_editable = ['new_building']
-    list_filter = ['new_building']
+    search_fields = ['town',]
+    readonly_fields = ['created_at',]
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town',]
+    list_editable = ['new_building',]
+    list_filter = ['new_building',]
+
+
+class ComplaintAdmin(admin.ModelAdmin):
+    raw_id_fields = ['flat', 'user']
 
 
 admin.site.register(Flat, FlatAdmin)
+admin.site.register(Complaint, ComplaintAdmin)
