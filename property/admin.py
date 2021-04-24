cb9d5ddb50d7ac38e32742583044e2b895bbba66
Michael Yudin <mihelitto@gmail.com>
2021-04-24 18:23:41 +0300

    Add flat owner editing to admin

--- property/admin.py
+++ property/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ['owner',]
+    verbose_name = 'Собственники'
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town',]
     readonly_fields = ['created_at',]
@@ -9,6 +15,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building',]
     list_filter = ['new_building',]
     raw_id_fields = ['likes',]
+    inlines = [OwnerInline,]
 
 
 class ComplaintAdmin(admin.ModelAdmin):
